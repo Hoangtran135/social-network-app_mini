1f3ec5be440ce702b046e3ac2350e48b703de4ef
Hoang Tran <hoangtranxuan04@gmail.com>
2026-09-24 16:24:20 +0700

    update cmt

--- src/context/ChatWindowsContext.tsx
+++ src/context/ChatWindowsContext.tsx
@@ -8,7 +8,7 @@ export interface ChatWindow {
   minimized: boolean;
 }
 
-const MAX_WINDOWS = 3;
+const MAX_WINDOWS = 4;
 
 function useChatWindowsState() {
   const [windows, setWindows] = useState<ChatWindow[]>([]);
